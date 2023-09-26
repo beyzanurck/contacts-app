5d06aa6ffb1e2b079d90713262323d128acbd87e
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-09-26 17:19:04 -0500

    displays contact details based on the person the user clicks

--- client/src/components/ContactDetail.jsx
+++ client/src/components/ContactDetail.jsx
@@ -6,12 +6,25 @@ export default function ContactDetail(props) {
   const { contactId } = useParams();
   let { state } = useLocation();
 
-  console.log(state)
+  // console.log(state)
+  // console.log(state.person.first_name)
 
   return (
     <div className='contact-detail'>
-      <p>{contactId}</p>
+
+      <img className="circle-img" src={state.person.image_path} alt="avatar_img" />
       
+      <h2>{state.person.first_name} {state.person.last_name}</h2>
+      
+      <p>{state.person.phone_number}</p>
+      <p>{state.person.email}</p>
+      <p>{state.person.address}</p>
+
+      <div className='edit-delete-buttons'>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    
     </div>
   )
 }
